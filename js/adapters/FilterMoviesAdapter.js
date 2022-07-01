41a2edba460f718227f3fc44edad4daafb08f5b4
A_Larose <larose.alain@gmail.com>
2022-07-01 03:09:18 +0200

    réalisation de l'exercice

--- js/adapters/FilterMoviesAdapter.js
+++ js/adapters/FilterMoviesAdapter.js
@@ -1,2 +1,10 @@
 class FilterMoviesAdapter {
+    constructor(Movies, actor) {
+        this._Movies = Movies
+        this._actor = actor
+    }
+
+    async filterByActor() {
+        return FilterV2.filterByActor(this._actor, this._Movies)
+    }
 }
