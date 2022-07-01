ce2f97194588912611d78071f5dfa8ceb03fad38
A_Larose <larose.alain@gmail.com>
2022-07-02 01:42:45 +0200

    réalistion de l'exercice

--- js/quiz/question-6.js
+++ js/quiz/question-6.js
@@ -6,10 +6,16 @@ class Book {
     }
 }
 
-const bookWithPrice = () => {}
+const bookWithPrice = () => {
+    Book._price = price
+    Book.givePrice = () => {
+    console.log(`${Book._title} coûte actuellement ${price} €`)
+    }
+    return Book
+}
 
 // J'instancie l'objet Book sans décorateur
-// const LesMiserables = new Book('Les Misérables', 'Victor Hugo', 'Drame')
+const LesMiserables = new Book('Les Misérables', 'Victor Hugo', 'Drame')
 
 // J'instancie l'objet Book décoré
-// const LesTroisMousquetaires = bookWithPrice(new Book('Les Trois Mousquetaires', 'Alexandre Dumas', 'Roman historique'), 17)
+const LesTroisMousquetaires = bookWithPrice(new Book('Les Trois Mousquetaires', 'Alexandre Dumas', 'Roman historique'), 17)
