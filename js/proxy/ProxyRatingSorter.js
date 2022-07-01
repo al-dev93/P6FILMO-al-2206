4b9fa61523c838b855528c97813634348991aed5
A_Larose <larose.alain@gmail.com>
2022-07-01 22:27:54 +0200

    réalisation de l'exercice

--- js/proxy/ProxyRatingSorter.js
+++ js/proxy/ProxyRatingSorter.js
@@ -3,6 +3,17 @@ class ProxyRatingSorter {
         this.cache = []
     }
 
-    async sorter() {
+    async sorter(movies, orderBy) {
+        const cachedData = this.cache.find(element => element.key === orderBy)
+
+        if(cachedData){
+            console.log('Get from cash')
+            return cachedData
+        }
+        
+        const data = await RatingSorterApi.sorter(movies, orderBy)
+
+        this.cache.push(data)
+        return data
     }
 }
