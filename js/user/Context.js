529bc0ee04f33d5fdbeca05475358fcae32c286d
A_Larose <larose.alain@gmail.com>
2022-07-04 15:03:11 +0200

    réalisation de l'exercice

--- js/user/Context.js
+++ js/user/Context.js
@@ -1,2 +1,28 @@
 class UserContext {
+    constructor(){
+        this.states = [
+            new AnonymousUserState(),
+            new UserConnectedState()
+        ]
+        this.currentState = this.getInitialState()
+    }
+
+    getInitialState() {
+        const user = new User()
+        // const [AnonymousUserState, UserConnectedState] = this.states
+        if(!user.user){
+            return this.states[0] // ou bien AnonymousUserState 
+        }
+        else {
+            return this.states[1] // ou bien UserConnectedState]
+        }
+    }
+
+    change(firstName, lastName) {
+        const user = new User({
+            firstName,
+            lastName
+        })
+        this.currentState = this.states[1]
+    }
 }
